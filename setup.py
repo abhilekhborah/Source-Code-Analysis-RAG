278d7a05fc90d5bd6176fe1b481c424e511ceac5
Abhilekh Borah <abhilekhkey@gmail.com>
2024-03-31 04:16:41 +0530

    commit new

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'SGA-RAG',
+    version= '1.0',
+    author= 'Abhilekh',
+    author_email= 'abhilekhkey@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
